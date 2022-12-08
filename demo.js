727e32e483139cad197283be00062aa23b9f739c
Imran940 <immoshaikh91@gmail.com>
2022-12-08 20:40:15 +0530

    added input features

---
+++ demo.js
@@ -0,0 +1,5 @@
+function save() {
+  let num1 = parseInt(document.getElementById("number1").value);
+  let num2 = parseInt(document.getElementById("number2").value);
+  document.getElementById("show-result").innerText = num1 + num2;
+}
